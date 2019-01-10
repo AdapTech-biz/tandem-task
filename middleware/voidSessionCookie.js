43c8ce64cebfc3c4b57623d3f53cae1f971e1286
xyd93 <xyd93@outlook.com>
2019-01-10 15:09:52 -0500

    Clear cookie upon logout

--- middleware/voidSessionCookie.js
+++ middleware/voidSessionCookie.js
@@ -2,13 +2,13 @@ var admin  = require( "firebase-admin");
 
 function voidSessionCookie (req, res, next) {
 
-    var sessionCookie = req.session.userCookie || '';
+    var sessionCookie = req.cookies.session || '';
     // Verify the session cookie. In this case an additional check is added to detect
     // if the user's Firebase session was revoked, user deleted/disabled, etc.
     admin.auth().verifySessionCookie(sessionCookie).then((decodedClaims) => {
         return admin.auth().revokeRefreshTokens(decodedClaims.sub);
     }).then(() => {
-        console.log('cookie revoked');
+        res.clearCookie('session');
         res.redirect('/');
     }).catch((error) => {
         console.log('error revoking cookie');
